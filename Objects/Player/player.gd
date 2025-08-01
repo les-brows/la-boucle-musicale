1ba2cc16e33056a4bdb4584ba90f7874a473f1f5
Nebulea <github@nebulea.dev>
2025-08-01 22:50:11 +0200

    feat: add moving character

---
+++ Objects/Player/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var speed = 200.0
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+
+	if Input.is_action_pressed("player1_right"):
+		direction.x += 1
+	if Input.is_action_pressed("player1_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("player1_down"):
+		direction.y += 1
+	if Input.is_action_pressed("player1_up"):
+		direction.y -= 1
+
+	# Normalize direction to prevent faster diagonal movement
+	if direction.length() > 0:
+		direction = direction.normalized()
+
+	velocity = direction * speed
+	move_and_slide()
