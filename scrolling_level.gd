616226c9f02709bd68e416cdcb7721f6f45a6a9e
Nebulea <github@nebulea.dev>
2025-08-01 23:36:12 +0200

    fix: add main scene

--- scrolling_level.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var camera = $Camera2D
-
-func move_camera():
-	camera.position = camera.position + Vector2(Globals.CAMERA_SPEED, 0)	
-
-func _process(delta):
-	move_camera()
