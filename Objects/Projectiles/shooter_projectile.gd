9c854694aba6bd227ca68676b62d4ed1a3f325d3
chloe <chloe200.durand@gmail.com>
2025-08-02 15:10:04 +0200

    projectile generique

--- Objects/Projectiles/shooter_projectile.gd
+++ Objects/Projectiles/shooter_projectile.gd
@@ -1,16 +1,3 @@
-extends Node2D
+extends Projectile
 
-class_name Projectile
-
-var curr_velocity = Vector2(0,0)
-
-
-func _on_shooter_projectile_duration_timer_timeout() -> void:
-	queue_free()
-
-func _physics_process(delta: float) -> void:
-	global_position += curr_velocity * delta
-	global_rotation = curr_velocity.angle()
-
-func set_velocity(target_velocity: Vector2):
-	curr_velocity = target_velocity
+class_name ProjectileShooter 
