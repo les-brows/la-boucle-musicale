3d0e66ea80baf3ffeb3d6e19357c2c1a51d18aa2
elias-bina <elias.bina58@gmail.com>
2025-08-02 11:53:57 +0200

    feat: Add new song + pitch management

--- Objects/Enemies/shooter.gd
+++ Objects/Enemies/shooter.gd
@@ -13,13 +13,13 @@ var shoot_partition: Partition
 
 func _init() -> void:
 	super()
-	var note1: Note = Note.new(0, 0, 0)
-	var note2: Note = Note.new(1, 0, 0)
-	var note3: Note = Note.new(3, 0, 0)
-	var note4: Note = Note.new(5, 0, -0.1)
-	var note5: Note = Note.new(6, 0, 0)
-	var list_notes: Array[Note] = [note1, note2, note3, note4, note5]
-	shoot_partition = Partition.new(4, 8, list_notes)
+	var list_notes: Array[Note] = [Note.new(0, 0, 3), Note.new(3, 0, -2), Note.new(5, 0, -2), 
+								   Note.new(7, 0, -2), Note.new(8, 0, 3), Note.new(11, 0, -2),
+								   Note.new(13, 0, -2), Note.new(14, 0, 4), Note.new(16, 0, 3),
+								   Note.new(16 + 3, 0, -2), Note.new(16 + 5, 0, -2), Note.new(16 + 7, 0, -2),
+								   Note.new(16 + 8, 0, 3), Note.new(16 + 11, 0, 6), Note.new(16 + 13, 0, 4),
+								   Note.new(16 + 14, 0, 1)]
+	shoot_partition = Partition.new(4, 32, list_notes)
 
 
 func _process(delta: float) -> void:
@@ -39,7 +39,7 @@ func _on_beat_launched(num_beat: int) -> void:
 	if(num_beat == shoot_partition.get_next_beat(num_beat)):
 		boing_state = NB_SECONDS_BOING_RECOVER + NB_SECONDS_BOING_JUMP
 		var note = shoot_partition.get_curr_note()
-		$InstrumentPlayer.pitch_scale = 1.0 + note.pitch
+		$InstrumentPlayer.pitch_scale = pow(2, note.pitch/12.0)
 		$InstrumentPlayer.play()
 		shoot_projectile(Vector2(-1, 0))
 		print("Boing ", note.beat_number, " ", note.pitch)
--- Rythm/note.gd
+++ Rythm/note.gd
@@ -4,9 +4,9 @@ class_name Note
 
 var beat_number: int = 0
 var action: int = 0
-var pitch: float = 0
+var pitch: int = 0
 
-func _init(init_beat_number: int, init_action: int, init_pitch: float):
+func _init(init_beat_number: int, init_action: int, init_pitch: int):
 	action = init_action
 	beat_number = init_beat_number
 	pitch = init_pitch
