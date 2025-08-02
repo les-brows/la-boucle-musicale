044138ec7c3a44c4c562c62c297e8f8a0a59165f
Nebulea <github@nebulea.dev>
2025-08-02 08:13:16 +0200

    wip

--- Levels/scrolling_level.gd
+++ Levels/scrolling_level.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 @onready var camera = $Camera2D
 @onready var player = $Player
+@onready var level_portions = $LevelPortions
+@onready var level_portion_previous = $LevelPortions/LevelPortionPrevious
+@onready var level_portion_current = $LevelPortions/LevelPortionCurrent
+@onready var level_portion_next = $LevelPortions/LevelPortionNext
+
+var LevelPortionScene = preload("res://Levels/LevelPortion.tscn")
+
 @onready var CAMERA_SIZE = get_viewport_rect().size
 
 func move_camera():
@@ -16,11 +23,25 @@ func clamp_player_inside_camera():
 func _process(delta):
 	move_camera()
 	clamp_player_inside_camera()
+	
+func _ready():
+	Globals.middle_level_reached.connect(_on_middle_level_reached)
+	Globals.end_level_reached.connect(_on_end_level_reached)
+	
+	level_portion_previous.position = -Vector2(Globals.LEVEL_SIZE, 0)
+	level_portion_next.position = Vector2(Globals.LEVEL_SIZE, 0)
+	
+	
 
-
-func _on_end_line_body_entered(body: Node2D) -> void:
-	print("Player hit the end of the level !")
-
-
-func _on_middle_line_body_entered(body: Node2D) -> void:
+func _on_middle_level_reached() -> void:
+	var level_portion_scene_instance = LevelPortionScene.instantiate()
+	level_portion_scene_instance.position = Vector2(Globals.LEVEL_SIZE, 0)
+	level_portions.add_child(level_portion_scene_instance)
+	
+	
+	for level_portion in level_portions.get_children():
+		level_portion.position = level_portion.position - Vector2(500,0)
 	print("Player hit the middle of the level !")
+	
+func _on_end_level_reached() -> void:
+	print("Player hit the end of the level !")
--- globals.gd
+++ globals.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var CAMERA_SPEED = 0.5
+var LEVEL_SIZE = 5000
 
 const BPM_SUBDIVISION : int = 8
 const BASE_BPM : int = 175
@@ -8,3 +9,6 @@ const BPM: int = BPM_SUBDIVISION * BASE_BPM
 
 @warning_ignore("unused_signal")
 signal beat_launched(num_beat: int)
+
+signal middle_level_reached()
+signal end_level_reached()
