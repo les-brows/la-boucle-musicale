7b72ec784d76b36972ee9a9d4524ad5de3fdbcee
elias-bina <elias.bina58@gmail.com>
2025-08-02 08:15:18 +0200

    feat: Subdivised beats

--- Rythm/test_sound.gd
+++ Rythm/test_sound.gd
@@ -1,6 +1,20 @@
 extends AudioStreamPlayer
 
+const VOLUME_DB_DIFF = 12
+const VOLUME_DB_DIFF_BAR = 6
+var initial_volume_db: float = 0
+
+
+func _init() -> void:
+	initial_volume_db = volume_db
 
 func _on_music_manager_beat_launched(num_beat: int) -> void:
+	if(num_beat % Globals.BPM_SUBDIVISION == 0):
+		if((num_beat / Globals.BPM_SUBDIVISION) % 4 ==0):
+			volume_db = initial_volume_db
+		else:
+			volume_db = initial_volume_db - VOLUME_DB_DIFF_BAR
+	else:
+		volume_db = initial_volume_db - VOLUME_DB_DIFF
 	play()
  
--- globals.gd
+++ globals.gd
@@ -2,4 +2,6 @@ extends Node
 
 var CAMERA_SPEED = 0.5
 
-var BPM : int = 175
+const BPM_SUBDIVISION : int = 8
+const BASE_BPM : int = 175
+const BPM: int = BPM_SUBDIVISION * BASE_BPM
