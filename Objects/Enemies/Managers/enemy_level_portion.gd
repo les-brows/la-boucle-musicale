468d7a7a4d7bf5e81b7a9a57b1dd44812a7106c7
elias-bina <elias.bina58@gmail.com>
2025-08-02 18:15:26 +0200

    feat: Better enemy spawn for level 1

--- Objects/Enemies/Managers/enemy_level_portion.gd
+++ Objects/Enemies/Managers/enemy_level_portion.gd
@@ -4,14 +4,40 @@ class_name EnemyLevelPortion
 
 var shooter_preload = preload("res://Objects/Enemies/Shooter.tscn");
 
-func _init() -> void:
-	for index in range(5):
-		var curr_shooter: Enemy = shooter_preload.instantiate()
-		curr_shooter.position = Vector2(randi_range(500, 4500), randi_range(100, 600))
-		add_child(curr_shooter)
+var loop: int = 0
 
+func _init() -> void:
+	enemy_generation()
+
+func set_loop(_loop: int):
+	loop = _loop
 
 func set_player(player: Player):
 	for enemy in get_children():
 		if enemy is Enemy:
 			enemy.set_player(player)
+			
+func enemy_generation():
+	match loop:
+		0:
+			#Cut the map in eight, with at least one enemy in each trunk
+			for index in range(8):
+				add_enemy(Vector2(randi_range(60, Globals.LEVEL_SIZE / 10 - 60) + (index + 2) * Globals.LEVEL_SIZE / 10, randi_range(Globals.BOUNDARY_LOW + 60, Globals.BOUNDARY_UP - 60)), EnemyType.SHOOTER)
+			pass
+		_:
+			for index in range(500):
+				var curr_shooter: Enemy = shooter_preload.instantiate()
+				curr_shooter.position = Vector2(randi_range(500, 4500), randi_range(Globals.BOUNDARY_LOW + 60, Globals.BOUNDARY_UP - 60))
+				add_child(curr_shooter)
+				print("NOT IMPLEMENTED GET DUNKED ON")
+
+
+func add_enemy(pos: Vector2, enemy_type: int):
+	var curr_enemy: Enemy = null
+	match enemy_type:
+		EnemyType.SHOOTER:
+			curr_enemy = shooter_preload.instantiate()
+	
+	
+	curr_enemy.position = pos
+	add_child(curr_enemy)
