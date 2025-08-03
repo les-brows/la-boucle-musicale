e18e0f4744e027f038dd0df04ca9a947022e3e7a
elias-bina <elias.bina58@gmail.com>
2025-08-03 13:42:14 +0200

    fix: Audio sync

--- Rythm/metronome.gd
+++ Rythm/metronome.gd
@@ -5,13 +5,35 @@ var next_beat_position: float = 60.0 / Globals.BPM
 var seconds_elapsed: float = 0.0
 var time_start: float = 0.0
 
+var start_metronome: bool = false
+var last_playback_pos: float = -1.0
+var num_loop: int = 0
+
+var audio_player: AudioStreamPlayer = null
+var stream_length_sec: float = 0
+
 func _init() -> void:
-	time_start = Time.get_unix_time_from_system()
+	pass
+
+func set_audio_node(_audio_player: AudioStreamPlayer):
+	audio_player = _audio_player
+	print(audio_player)
+	stream_length_sec = audio_player.stream.get_length()
+	time_start = wrap_get_playback_position()
 	last_beat = 0
+	start_metronome = true
 
 func _process(_delta: float) -> void:
-	seconds_elapsed = Time.get_unix_time_from_system() - time_start
-	while(next_beat_position < seconds_elapsed):
-		Globals.beat_launched.emit(last_beat)
-		last_beat += 1
-		next_beat_position = last_beat *  60.0 / Globals.BPM
+	if(start_metronome):
+		seconds_elapsed = wrap_get_playback_position() - time_start
+		while(next_beat_position < seconds_elapsed):
+			Globals.beat_launched.emit(last_beat)
+			last_beat += 1
+			next_beat_position = last_beat *  60.0 / Globals.BPM
+
+func wrap_get_playback_position() -> float:
+	var playback_pos: float = audio_player.get_playback_position()
+	if(playback_pos < last_playback_pos):
+		num_loop+=1
+	last_playback_pos = playback_pos
+	return num_loop * stream_length_sec + playback_pos
--- Rythm/music_manager.gd
+++ Rythm/music_manager.gd
@@ -1,1 +1,4 @@
 extends Node
+
+func _ready() -> void:
+	$Metronome.set_audio_node($Music)
